cd0bf312d8c264f7142e8ee54327d46a6b186975
99003511 <santhrupthi.k@ltts.com>
2021-03-08 08:49:35 +0530

    Update main.h

--- box_array/INC/main.h
+++ box_array/INC/main.h
@@ -6,11 +6,13 @@
 //int insert(struct box *b2,int increment,int update_id,float update_length,float update_breadth,float update_height,float update_weight,char update_color[20]);
  //int search(struct box *b2,int x,int search_id);
  //float max_height(struct box *ptr);
+
  struct box{
  int unique_id;
  float length,breadth,height,weight;
  char color[20];
 };
+
 //int increment=0;
 /**
  * C program to delete an element from array at specified position
